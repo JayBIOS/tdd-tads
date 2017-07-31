56cf35b11bb542115047ee10abb53b36e3200b6a
Jonathan Batista <jonathan.b.d.o.s@gmail.com>
2017-07-31 09:44:34 -0300

    Implementar classe Ano

--- src/Ano.php
+++ src/Ano.php
@@ -2,8 +2,26 @@
 
 class Ano
 {
-	function valor() {}
-	function bissexto() {}
+	private $valor;
+
+	function __construct($valor)
+	{
+		$this->valor = $valor;
+	}
+
+	function valor() {
+		return $this->valor;
+	}
+
+	function bissexto() {
+		if($this->valor % 4 == 0) {
+			if($this->valor % 100 == 0 && $this->valor % 400 != 0) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
 }
 
 ?>
--- test/AnoTest.php
+++ test/AnoTest.php
@@ -5,46 +5,32 @@ use PHPUnit\Framework\TestCase;
 class AnoTest extends TestCase
 {
 	public function testAnoInvalido() {
-		$stub = $this->createMock(Ano::class);
+		$stub = new Ano(-1);
 		
-		$stub->method('valor')->will($this->returnValue(-1));
-
 		$this->assertLessThan(0, $stub->valor());
 	}
 
 	public function testNaoDivisivelPorQuatro() {
-		$stub = $this->createMock(Ano::class);
+		$stub = new Ano(1742);
 		
-		$stub->method('valor')->will($this->returnValue(1742));
-		$stub->method('bissexto')->will($this->returnValue(false));
-
 		$this->assertFalse($stub->bissexto());
 	}
 	
 	public function testDivisivelPorQuatroEPorCem() {
-		$stub = $this->createMock(Ano::class);
+		$stub = new Ano(1500);
 		
-		$stub->method('valor')->will($this->returnValue(1500));
-		$stub->method('bissexto')->will($this->returnValue(false));
-
 		$this->assertFalse($stub->bissexto());	
 	}
 
 	public function testDivisivelPorQuatroENaoPorCem() {
-		$stub = $this->createMock(Ano::class);
-		
-		$stub->method('valor')->will($this->returnValue(1996));
-		$stub->method('bissexto')->will($this->returnValue(true));
+		$stub = new Ano(1996);
 
 		$this->assertTrue($stub->bissexto());	
 	}
 
 	public function testDivisivelPorQuatroEDivisivelPorQuatrocentos() {
-		$stub = $this->createMock(Ano::class);
+		$stub = new Ano(1600);
 		
-		$stub->method('valor')->will($this->returnValue(1600));
-		$stub->method('bissexto')->will($this->returnValue(true));
-
 		$this->assertTrue($stub->bissexto());	
 	}
 }
